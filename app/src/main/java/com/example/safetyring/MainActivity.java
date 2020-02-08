287eeccf466ac636d0c6cb18bd27c96b2e09ca3e
saumya <saumyaguptaa008@gmail.com>
2020-02-09 02:11:09 +0530

    Splash screen added

--- app/src/main/java/com/example/safetyring/MainActivity.java
+++ app/src/main/java/com/example/safetyring/MainActivity.java
@@ -2,13 +2,51 @@ package com.example.safetyring;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ProgressBar;
+
+import com.felipecsl.gifimageview.library.GifImageView;
+
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
+    private GifImageView gifImageView;
+    private ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        gifImageView = (GifImageView) findViewById(R.id.gifimage);
+        //progressBar = (ProgressBar) findViewById(R.id.progress);
+
+        try {
+            InputStream inputStream = getAssets().open("img.gif");
+            byte[] bytes = IOUtils.toByteArray(inputStream);
+            gifImageView.setBytes(bytes);
+            gifImageView.startAnimation();
+        } catch (IOException ex) {
+
+        }
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                finish();
+            }
+        },5000);
     }
+
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//
+//
+//    }
 }
