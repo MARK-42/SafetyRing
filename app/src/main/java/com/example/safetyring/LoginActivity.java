b37330b85979527ba2389d0f762520fcd603aba1
Ritik Raj <ritikgupta15@ymail.com>
2020-02-08 14:53:18 +0530

    login and registration done

--- app/src/main/java/com/example/safetyring/LoginActivity.java
+++ app/src/main/java/com/example/safetyring/LoginActivity.java
@@ -1,14 +1,30 @@
 package com.example.safetyring;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        auth = FirebaseAuth.getInstance();
+        if(auth.getCurrentUser()!=null){
+            startActivity(new Intent(LoginActivity.this,UserActivity.class));
+        }
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.FragmentContainer,new LoginFragment());
+        fragmentTransaction.commit();
+
     }
 }
